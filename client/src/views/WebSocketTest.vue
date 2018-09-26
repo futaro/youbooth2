89ecb41b117d18b2876c8f792f0abb836606ef56
Masashi Machida <masashi.machida@aiasahi.jp>
2018-09-27 00:11:25 +0900

    first commit

---
+++ client/src/views/WebSocketTest.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>hoge</h1>
+</template>
+<script>
+
+  export default {
+    name : "WebSocketTest",
+
+    created() {
+      var ws = new WebSocket('ws://localhost:7000/', ['echo-protocol', 'soap', 'xmpp']);
+
+      ws.onopen = function () {//WS接続確立
+        ws.send('hello');
+      };
+
+      // Log errors
+      ws.onerror = function (error) {
+        console.log('WebSocket Error ' + error);
+      };
+
+      // Log messages from the server
+      ws.onmessage = function (e) {
+        console.log('Server: ' + e.data);
+      };
+
+    }
+  }
+</script>
