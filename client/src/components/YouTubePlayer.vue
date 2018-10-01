d93ede8f303749773d1ec37f04b93e3b84336ea9
Masashi Machida <masashi.machida@aiasahi.jp>
2018-10-01 13:46:27 +0900

    youtube

--- client/src/components/YouTubePlayer.vue
+++ client/src/components/YouTubePlayer.vue
@@ -1,5 +1,6 @@
 <template>
-  <iframe :src="src" @load="onLoad" type="text/html" ref="player" frameborder="0" allowfullscreen></iframe>
+  <div id="player"></div>
+  <!--<iframe :src="src" @load="onLoad" type="text/html" ref="player" frameborder="0" allowfullscreen></iframe>-->
 </template>
 
 <script>
@@ -9,39 +10,46 @@
 
     props: ['value', 'from'],
 
-    computed: {
-      src() {
-        if (this.value) {
-          return `http://www.youtube.com/embed/${this.value}?enablejsapi=1&startSeconds=1&startSeconds=${this.from}&origin=http://y.futa.ro`
-        } else {
-          return null
+    data() {
+      return {
+        player: null
+      }
+    },
+
+    // computed: {
+    //   src() {
+    //     if (this.value) {
+    //       return `http://www.youtube.com/embed/${this.value}?enablejsapi=1&autoplay=1&startSeconds=${this.from}&origin=http://y.futa.ro`
+    //     } else {
+    //       return null
+    //     }
+    //   }
+    // },
+
+    watch: {
+      value(val) {
+        if (val) {
+          this.player = new window.YT.Player('player', {
+            videoId: val,
+            events : {
+              'onReady'      : this.onPlayerReady,
+              'onStateChange': this.onPlayerStateChange
+            }
+          })
         }
       }
     },
 
-    // created() {
-    //   window.removeEventListener('message', e => this.onMessage(e))
-    //   window.addEventListener('message', e => this.onMessage(e))
-    // },
-
     methods: {
 
-      onMessage(e) {
-        // if (e.origin === 'http://embwwwed.youtube.com') {
-        //   if (e.data.eventName === 'playerStatusChange') {
-        //     console.log(e.data.data.playerStatus)
-        //     if (e.data.data.playerStatus === 4) {
-        //       this.$emit('input', null)
-        //     }
-        //   }
-        // }
+      onPlayerReady(event) {
+        event.target.playVideo();
       },
 
-      onLoad() {
-        // this.$refs.player.contentWindow.postMessage({
-        //   sourceConnectorType: 1,
-        //   eventName          : 'play'
-        // }, 'http://embed.nicovideo.jp/')
+      onPlayerStateChange(event) {
+        if (event.data === window.YT.PlayerState.ENDED) {
+          this.$emit('input', null)
+        }
       }
     }
   }
