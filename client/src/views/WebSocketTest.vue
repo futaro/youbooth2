52bc85e5d0d3a93f58b9084628cedc84cafbe079
Masashi Machida <masashi.machida@aiasahi.jp>
2018-10-01 13:16:59 +0900

    youtube

--- client/src/views/WebSocketTest.vue
+++ client/src/views/WebSocketTest.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
     <NicoPlayer v-if="nico_uid" v-model="nico_uid" :from="from" />
+    <YouTubePlayer v-if="youtube_uid" v-model="nico_uid" :from="from" />
   </div>
 </template>
 <script>
 
-  import NicoPlayer from '../components/NicoPlayer'
-  import configs    from '../../../config.client'
+  import NicoPlayer    from '../components/NicoPlayer'
+  import YouTubePlayer from '../components/YouTubePlayer'
+  import configs       from '../../../config.client'
 
   export default {
 
     name: "WebSocketTest",
 
-    components: {NicoPlayer},
+    components: {NicoPlayer, YouTubePlayer},
 
     data() {
       return {
-        ws       : null,
-        nico_uid : null,
-        from     : 0,
-        workspace: null,
-        channel  : null
+        ws         : null,
+        nico_uid   : null,
+        youtube_uid: null,
+        from       : 0,
+        workspace  : null,
+        channel    : null
       }
     },
 
@@ -67,6 +70,10 @@
         if (data.track.type === 'niconico') {
           this.nico_uid = data.track.uid
           this.from     = data.from
+        }
+        if (data.track.type === 'youtube') {
+          this.youtube_uid = data.track.uid
+          this.from        = data.from
         }
       }
     }
